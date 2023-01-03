f499b2916f99b41b9844b0f3a2873fbc9cbabef9
Federico Luna <fluna1973@yahoo.com>
2023-01-03 14:13:36 -0600

    react app complete

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
+import { fetchGreeting } from './redux/greetings/greetingsSlice';
+
+store.dispatch(fetchGreeting());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
 
